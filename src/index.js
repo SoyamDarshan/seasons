c4757fb5da02b82d49f614e3d90375d48c8480c2
Soyam Darshan Pattanaik <32511312+SoyamDarshan@users.noreply.github.com>
2019-06-24 01:30:43 +0530

    added class based implementation for state

--- src/index.js
+++ src/index.js
@@ -1,9 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+/*
+//Function based implementation
 const App = () => {
-    return <div>Hi there!</div>;
+    window.navigator.geolocation.getCurrentPosition(
+        (position) => console.log(position),
+        (err) => console.log(err)
+    );
+    return <div>Latitude: </div>;
 };
+*/
+//Class based implementation
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        
+        //The only time we do direct assignment to this.state
+        this.state = { lat: null };
+    
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => { 
+                this.setState({ lat: position.coords.latitude })
+            },
+            (err) => console.log(err)
+        );
+    }
+
+    // React says we have to define render!
+    render() {
+        return <div>Latitude: { this.state.lat }</div>
+    }
+}
 
 ReactDOM.render(
     <App />, document.querySelector('#root')
