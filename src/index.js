3e34f1a0c3c589f6e4baa24ab09ea07510936c25
Soyam Darshan Pattanaik <32511312+SoyamDarshan@users.noreply.github.com>
2019-06-25 01:18:55 +0530

    updated syntaxes

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
 /*
 //Function based implementation
 const App = () => {
@@ -14,34 +15,38 @@ const App = () => {
 //Class based implementation
 class App extends React.Component {
 
-    constructor(props) {
-        super(props);
-        
-        //The only time we do direct assignment to this.state
-        this.state = { lat: null, errorMessage: '' };
-    
+    // constructor equivalent
+    state = { lat: null, errorMessage: ''};
+
+    componentDidMount() {
+        console.log('My component was rendered to the screen');
         window.navigator.geolocation.getCurrentPosition(
-            (position) => { 
-                // We called setState
-                this.setState({ lat: position.coords.latitude })
-            },
+            // We called setState
+            position => { this.setState({ lat: position.coords.latitude })},
             //err => console.log(err)
-            err => {
-                this.setState({ errorMessage: err.message });
-            }
+            err => {this.setState({ errorMessage: err.message });}
         );
+    }
+
+    componentDidUpdate() {
+        console.log('My component was updated on the screen')
+    }
+
+    renderContent(){
+        if ( this.state.errorMessage && !this.state.lat ){
+            return <div>Error: { this.state.errorMessage }</div>;
+        }
+        if ( !this.state.errorMessage && this.state.lat ){
+            return <SeasonDisplay lat={this.state.lat} />;
+        }
+        return <Spinner message="Please accept location request"/>;
     }
 
     // React says we have to define render!
     render() {
-
-            if (this.state.errorMessage && !this.state.lat){
-                return <div>Error: { this.state.errorMessage }</div>;
-            }
-            if (!this.state.errorMessage && this.state.lat ){
-                return <div>Latitude: { this.state.lat }</div>;
-            }
-            return <div>Loading!</div>;
+     return (
+         <div className="border red">{ this.renderContent() }</div>
+     );       
     }
 }
 
